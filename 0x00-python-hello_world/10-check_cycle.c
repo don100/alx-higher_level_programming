bba953faf1650effd994023d18487787857dca61
don100 <hajjaj.chiheb1@gmail.com>
2023-06-06 16:45:03 -0700

    10-1

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * check_cycle - a function in C that checks
+ * if a singly linked list has a cycle in it
+ * @list: - pointer to list
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *nodes[1000];
+	int i, j, k;
+	if (list == NULL)
+		return (0);
+	i = 0;
+	while (list->next != NULL)
+	{
+		nodes[i] = list;
+		j = 0;
+		k = i;
+		while (1)
+		{
+			if (list->next == nodes[j])
+				return (1);
+			if (list->next == nodes[k])
+				return (1);
+			if (k == (i / 2))
+				break;
+			j++;
+			k--;
+		}
+		list = list->next;
+		i++;
+	}
+	return (0);
+}
