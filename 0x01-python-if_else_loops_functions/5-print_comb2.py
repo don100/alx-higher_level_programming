45650acc28601d393aeada94b37244fcac7cf9f3
don100 <hajjaj.chiheb1@gmail.com>
2023-06-08 18:48:22 +0200

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for a in range(0, 100):
     num = str(a) if a >= 10 else '0'+str(a)
-    num = num+", " if a < 99 else num
+    num += ", " if a < 99 else '\n'
     print("{0}".format(num), end='')
