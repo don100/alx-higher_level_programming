e9319c9232b7a94c8109080457ea34d7971cb11e
don100 <hajjaj.chiheb1@gmail.com>
2023-06-08 19:05:31 +0200

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,4 @@
 for a in range(0, 10):
     for b in range(a+1, 10):
         num = "\n" if (a == 8 and b == 9) else ", "
-        print("{0}{1}{2}".format(a,b,num), end='')
+        print("{0}{1}{2}".format(a, b, num), end='')
