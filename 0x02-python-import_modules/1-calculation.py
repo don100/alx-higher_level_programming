038286cab41341f7d8ba485b6f9acc4df2f81cc6
don100 <hajjaj.chiheb1@gmail.com>
2023-06-09 20:20:37 +0200

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -7,4 +7,3 @@ b = 5
 print("{} + {} = {:d}".format(a, b, add(a, b)))
 print("{} + {} = {:d}".format(a, b, sub(a, b)))
 print("{} + {} = {:d}".format(a, b, mul(a, b)))
-print("{} + {} = {:d}".format(a, b, div(a, b)))
