342a890b2fa277ff30858d939b2ccfe619fb122e
don100 <hajjaj.chiheb1@gmail.com>
2023-06-15 00:09:56 +0200

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ != "__main__":
     exit()
+
 def square_matrix_simple(matrix=[]):
-    x = [[int(y**2) for y in x] for x in matrix]
-    return x
+    return [[int(y**2) for y in x] for x in matrix]
