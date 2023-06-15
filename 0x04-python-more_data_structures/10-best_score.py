9ea835c0ea7f368789e794580d559484e4eaf0f0
don100 <hajjaj.chiheb1@gmail.com>
2023-06-15 19:37:40 +0200

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    best_key = None
-    best = ""
     if a_dictionary is None:
-        return best_key
-    best_key = 0
-    for x, y in a_dictionary.items():
-        if y > best_key:
-            best_key = y
-            best = x
-    return best
+        return None
+    else:
+        best_key = 0
+        best = ""
+        for x, y in a_dictionary.items():
+            if y > best_key:
+                best_key = y
+                best = x
+        return best
