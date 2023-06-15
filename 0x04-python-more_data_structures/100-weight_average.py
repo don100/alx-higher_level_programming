fbe6a74f5b8a1e206884d26a8a719d419fa846b1
don100 <hajjaj.chiheb1@gmail.com>
2023-06-15 23:33:11 +0200

    Update 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if not my_list:
+        return 0
     a = sum(map(lambda x: x[0]*x[1], my_list))
     b = sum(map(lambda x: x[1], my_list))
     result = a/b
