f6d9db4654a9c933105f0d99cec800b13233e56c
don100 <hajjaj.chiheb1@gmail.com>
2023-06-15 03:06:13 +0200

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -4,10 +4,9 @@ def uniq_add(my_list=[]):
     for i in set(my_list):
         s += i
     return s
-'''def uniq_add(my_list=[]):
-    s = 0
-    for i in range(len(my_list)):
-        if my_list[i] not in my_list[i+1:]:
-            s += my_list[i]
-    return s
-'''
+#def uniq_add(my_list=[]):
+#    s = 0
+#   for i in range(len(my_list)):
+#      if my_list[i] not in my_list[i+1:]:
+#         s += my_list[i]
+#    return s
