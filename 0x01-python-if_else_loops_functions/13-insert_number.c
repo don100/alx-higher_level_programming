7f96d8ca163784e0ec3a4944b19a802f7fdfd70b
don100 <hajjaj.chiheb1@gmail.com>
2023-06-19 03:51:02 +0200

    Update 13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,1 +1,45 @@
+#include "lists.h"
 
+/**
+ * check_cycle - checks if a linked list contains a cycle
+ * @list: linked list to check
+ *
+ * Return: 1 if the list has a cycle, 0 if it doesn't
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+    listint_t *new;
+    listint_t *current;
+    listint_t *prev;
+
+    current = *head;
+
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+        return (NULL);
+
+    new->n = number;
+    //new->next = NULL;
+
+    if (*head == NULL){
+        new->next = NULL;
+        *head = new;
+    }
+        
+    else
+    {
+        while (current->next != NULL)
+        {
+            if(current->n >= number)
+            {
+                new->next = current;
+                prev->next = new;
+            }       
+            prev = current;
+            current = current->next;
+
+        }
+    }
+
+    return (new);
+}
