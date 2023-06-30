c9c241af0a6ef633dd018611690f0a02eda4cd17
don100 <hajjaj.chiheb1@gmail.com>
2023-06-30 14:28:29 -0700

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -10,8 +10,8 @@ class Square:
         if size < 0:
             raise Exception("size must be >= 0")
         if (len(position) != 2 or type(position[0]) != int or
-            type(position[1]) != int or position[0] < 0 or
-            position[1] < 0):
+                type(position[1]) != int or position[0] < 0 or
+                position[1] < 0):
             raise Exception("position must be a tuple of 2 positive integers")
         self.__size = size
         self.__position = position
@@ -35,8 +35,7 @@ class Square:
     @position.setter
     def position(self, value):
         if (len(value) != 2 or type(value[0]) != int or
-        type(value[1]) != int or value[0] < 0 or
-        value[1] < 0):
+                type(value[1]) != int or value[0] < 0 or value[1] < 0):
             raise Exception("position must be a tuple of 2 positive integers")
         self.__position = value
 
