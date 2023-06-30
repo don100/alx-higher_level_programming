3a26b6600317dbd5a38874315906f8c1785848aa
don100 <hajjaj.chiheb1@gmail.com>
2023-06-30 21:14:25 +0200

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -10,12 +10,18 @@ class Square:
         if size < 0:
             raise Exception("size must be >= 0")
         self.__size = size
-    
+
+    @property
     def size(self):
         return self.__size
 
+    @size.setter
     def size(self, value):
+        if type(value) != int:
+            raise Exception("size must be an integer")
+        if value < 0:
+            raise Exception("size must be >= 0")
         self.__size = value
-    
+
     def area(self):
         return self.__size*self.__size
