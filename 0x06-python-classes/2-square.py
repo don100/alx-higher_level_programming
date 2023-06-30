619899e6355f26e81878ea88d2dc4957842c997a
don100 <hajjaj.chiheb1@gmail.com>
2023-06-30 19:48:11 +0200

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,9 +4,10 @@
 
 class Square:
     """Content class square."""
+    
     def __init__(self, size=0):
         if(type(size) != int):
             raise Exception("size must be an integer")
-        if(size <= 0):
+        if(size < 0):
             raise Exception("size must be >= 0")
         self.__size = size
