241298704c48a63b92c4d98cfb0bcbbb9e1c750d
don100 <hajjaj.chiheb1@gmail.com>
2023-07-03 23:26:51 +0200

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -8,11 +8,11 @@ if len(sys.argv) != 2:
 try:
     N = int(sys.argv[1])
 except Exception:
-    print(f"{N} must be a number")
+    print("N must be a number")
     exit(1)
 
 if N < 4:
-    print(f"{N} must be at least 4")
+    print("N must be at least 4")
     exit(1)
 
 a = 0
