2455be0688ff22ad5ae532baf67e107fa65cf55c
don100 <hajjaj.chiheb1@gmail.com>
2023-07-04 19:00:38 +0200

    Update 100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    i = __import__('__main__').i
+    i = getattr(__import__('__main__'), [(i) for i in dir(__import__('__main__')) if i[:2] != "__" and i != "magic_string"][0])
     return ("BestSchool, " * (i+1))[:-2]
