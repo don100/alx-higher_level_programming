5363e916f3706b8a1c8031dde11d0d4161162f3a
don100 <hajjaj.chiheb1@gmail.com>
2023-07-04 05:07:08 +0200

    Update 101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,9 +4,8 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        if fct is None or args is None:
-            raise Exception
-        return fct(args[0], args[1])
+        a = fct(args[0], args[1])
+        return a
     except Exception as e:
         print(f"Exception: {e}", file=sys.stderr)
         return None
