5ab71f3d69c477182322aac6dadb90a0eec86220
don100 <hajjaj.chiheb1@gmail.com>
2023-07-07 03:53:47 +0200

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 '''Prototype: def add_integer(a, b=98):
-a and b must be integers or floats, otherwise raise 
-a and b must be first casted to integers if they are float
+a and b must be integers or floats
+a and b must be first casted to
 Returns an integer: the addition of a and b
-You are not allowed to import any module'''
+You are not allowed to import '''
 def add_integer(a, b=98):
     '''Prototype: def add_integer(a, b=98):
-    a and b must be integers or floats, otherwise raise 
-    a and b must be first casted to integers if they '''
+    a and b must be integers or floats,
+    a and b must be first casted to '''
     if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
     if type(b) != int and type(b) != float:
