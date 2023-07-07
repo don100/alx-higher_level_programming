bb40de6561fae927f883afad7f107f30146e60a5
don100 <hajjaj.chiheb1@gmail.com>
2023-07-07 03:03:04 +0200

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-'''
-Prototype: def add_integer(a, b=98):
+'''Prototype: def add_integer(a, b=98):
 a and b must be integers or floats, otherwise raise a TypeError exception with the message a must be an integer or b must be an integer
 a and b must be first casted to integers if they are float
 Returns an integer: the addition of a and b
-You are not allowed to import any module
-'''
+You are not allowed to import any module'''
 def add_integer(a, b=98):
-    '''
-    Prototype: def add_integer(a, b=98):
+    '''Prototype: def add_integer(a, b=98):
     a and b must be integers or floats, otherwise raise a TypeError exception with the message a must be an integer or b must be an integer
-    a and b must be first casted to integers if they are floatReturns an integer: the addition of a and b
-    '''
+    a and b must be first casted to integers if they are floatReturns an integer: the addition of a and b'''
     if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
     if type(b) != int and type(b) != float:
