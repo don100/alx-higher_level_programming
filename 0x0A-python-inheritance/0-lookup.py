b3576be4224e8754bb7eb2d029af6f9853df46ab
don100 <hajjaj.chiheb1@gmail.com>
2023-07-10 23:52:45 +0200

    Update 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 ''' 0. Lookup '''
 
+
 def lookup(obj):
     ''' Write a function that returns the list of available attributes and methods of an object: '''
     return dir(obj)
