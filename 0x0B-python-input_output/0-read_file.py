d833160799d4c11ca33b679c70e74f146e9a2317
don100 <hajjaj.chiheb1@gmail.com>
2023-07-12 01:33:48 +0200

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+''' 0-read_file.py '''
 
 def read_file(filename=""):
+    '''  Read file '''
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
     print(read_data)
