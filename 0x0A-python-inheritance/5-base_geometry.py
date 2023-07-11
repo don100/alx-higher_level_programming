3f2e65e88c108cd7a27876eacf9d7f608da39741
don100 <hajjaj.chiheb1@gmail.com>
2023-07-11 03:04:49 +0200

    Update 5-base_geometry.py

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""6-base_geometry.py"""
+"""5-base_geometry.py"""
 
 
 class BaseGeometry():
     """class BaseGeometry"""
-    def area(self):
-        raise Exception('area() is not implemented')
+    pass
