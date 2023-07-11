c20cb39d35045667e848f2fb566912a84b53309e
don100 <hajjaj.chiheb1@gmail.com>
2023-07-11 20:05:59 +0200

    Update 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,24 @@
-***
+#!/usr/bin/python3
+"""100-my_int.py"""
+
+
+class MyInt(int):
+    """class MyInt"""
+
+    def __eq__(self, other):
+        return not(self == other)
+
+    def __ne__(self, other):
+        return not(self != other)
+
+    def __lt__(self, other):
+        return not(self < other)
+
+    def __gt__(self, other):
+        return not(self > other)
+
+    def __le__(self, other):
+        return not(self <= other)
+
+    def __ge__(self, other):
+        return not(self >= other)
