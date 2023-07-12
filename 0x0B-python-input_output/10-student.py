62ea0bfed9957702bc14f22c966b7e6619503d77
don100 <hajjaj.chiheb1@gmail.com>
2023-07-12 16:01:38 +0200

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -16,6 +16,6 @@ class Student:
             for i in self.__dict__.keys():
                 if i in attrs:
                     mydict[i] = self.__dict__.get(i)
-            return mydict
+            return dict(mydict)
         else:
             return self.__dict__
