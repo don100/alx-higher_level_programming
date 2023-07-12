d1cc76ff48b44a58e23b828ffc95e984192b4cb7
don100 <hajjaj.chiheb1@gmail.com>
2023-07-12 13:59:33 +0200

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,16 +3,17 @@
 import json
 import sys
 
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-
-list_args = sys.argv[1:]
-filename = "add_item.json"
-
-try:
-    list_args = list(load_from_json_file(filename)) + list_args
-except FileNotFoundError:
-    save_to_json_file([], filename)
-    exit()
-
-save_to_json_file(list_args, filename)
+if __name__ == "__main__":
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    
+    list_args = sys.argv[1:]
+    filename = "add_item.json"
+    
+    try:
+        list_args = list(load_from_json_file(filename)) + list_args
+    except FileNotFoundError:
+        save_to_json_file([], filename)
+        exit()
+    
+    save_to_json_file(list_args, filename)
