8eb225840964d45023d29b3b9cf140f1b9293514
don100 <hajjaj.chiheb1@gmail.com>
2023-07-12 05:38:25 +0200

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,1 +1,18 @@
-***
+#!/usr/bin/python3
+''' module python3 '''
+import json
+import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+
+list_args = sys.argv[1:]
+filename = "add_item.json"
+
+try:
+    load_from_json_file(filename)
+except IOError:
+    save_to_json_file([], filename)
+    exit()
+list_args = list(load_from_json_file(filename)) + list_args
+save_to_json_file(list_args, filename)
