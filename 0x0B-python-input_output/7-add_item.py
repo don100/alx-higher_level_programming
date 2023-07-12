eb2972c0df67964417ab2e44a8b466d30c9d511c
don100 <hajjaj.chiheb1@gmail.com>
2023-07-12 13:54:12 +0200

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,9 +9,11 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 list_args = sys.argv[1:]
 filename = "add_item.json"
+
 try:
     list_args = list(load_from_json_file(filename)) + list_args
 except IOError:
     save_to_json_file([], filename)
     exit()
+
 save_to_json_file(list_args, filename)
