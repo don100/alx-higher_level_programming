2b4ec1dde835ce5fea4efd553dac0da8cb1d1eab
don100 <hajjaj.chiheb1@gmail.com>
2023-07-22 00:58:36 +0200

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -60,3 +60,31 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError('y must be >= 0')
         self.__y = value
+
+    def area(self):
+        return self.width * self.height
+
+    def display(self):
+        if self.y > 0:
+            print("\n" * (self.y - 1))
+        for i in range(self.height):
+            print(" " * self.x, end='')
+            print("#" * self.width)
+
+    def __str__(self):
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} \
+- {self.width}/{self.height}"
+
+    def update(self, *args, **kwargs):
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
+        for i, j in kwargs.items():
+            setattr(self, i, j)
