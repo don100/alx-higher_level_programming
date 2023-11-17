e4c3ae8cb9239715dd71d65fc521db8f3e1acd4e
don100 <hajjaj.chiheb1@gmail.com>
2023-11-17 18:09:22 +0100

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let x in arr) {
   console.log(arr[x]);
 }
