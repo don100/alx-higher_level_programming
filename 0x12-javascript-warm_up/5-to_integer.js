a1825b42083ebfb3e436aeaa892a698a273fd93b
don100 <hajjaj.chiheb1@gmail.com>
2023-11-17 15:35:48 +0100

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv;
-if(parseInt(args[2])) {
-  console.log('My number: ' + args[2])
+if (parseInt(args[2])) {
+  console.log('My number: ' + args[2]);
 } else {
-  console.log('Not a number')
+  console.log('Not a number');
 }
