d5c160a82e85b4c9900d3e1f9a93835e74e35af3
don100 <hajjaj.chiheb1@gmail.com>
2023-11-18 14:56:58 +0100

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const args = process.argv;
-if(args.length > 2){
+if (args.length > 2) {
   for (let x = 0; x < parseInt(args[2]); x++) {
-  console.log('C is fun');
-}
+    console.log('C is fun');
+  }
 } else {
   console.log('Missing number of occurrences');
 }
