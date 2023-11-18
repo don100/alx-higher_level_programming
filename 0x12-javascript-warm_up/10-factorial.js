c4d0ed177c70730ca386a5083c16e0c0b865f96c
don100 <hajjaj.chiheb1@gmail.com>
2023-11-18 17:00:19 +0100

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 const args = process.argv;
-let x = '';
+let x = 1;
 if (parseInt(args[2])) {
-  for (let i = 0; i < parseInt(args[2]); i++) {
+  for (let i = 1; i <= parseInt(args[2]); i++) {
     x *= i;
   }
-  for (let j = 0; j < parseInt(args[2]); j++) {
     console.log(x);
-  }
 } else {
-  console.log('Missing size');
+  console.log(1);
 }
