dab7e010ef58704be20fb550c189818a633d2b8b
don100 <hajjaj.chiheb1@gmail.com>
2023-11-18 16:45:21 +0100

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,7 +3,7 @@ const args = process.argv;
 let x = '';
 if (parseInt(args[2])) {
   for (let i = 0; i < parseInt(args[2]); i++) {
-    x += 'X';
+    x *= i;
   }
   for (let j = 0; j < parseInt(args[2]); j++) {
     console.log(x);
