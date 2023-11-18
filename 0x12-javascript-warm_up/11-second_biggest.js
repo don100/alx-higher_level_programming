238261773592699018607b9046ef4733b8254560
don100 <hajjaj.chiheb1@gmail.com>
2023-11-18 17:44:00 +0100

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,12 +4,12 @@ const args = process.argv;
 if (args.length <= 3) {
   console.log(0);
 } else {
-  let max1 = args[2];
-  let max2 = args[3];
+  let max1 = parseInt(args[2]);
+  let max2 = parseInt(args[3]);
   for (let i = 3; i < args.length; i++) {
-    if (max1 < args[i]) {
+    if (max1 < parseInt(args[i])) {
       max2 = max1;
-      max1 = args[i];
+      max1 = parseInt(args[i]);
     }
   }
   console.log(max2);
