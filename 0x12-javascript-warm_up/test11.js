05000eb8038902c8ed5b6578fc37a6bb66e5cc08
don100 <hajjaj.chiheb1@gmail.com>
2023-11-18 17:32:48 +0100

    Update test11.js

--- 0x12-javascript-warm_up/test11.js
+++ 0x12-javascript-warm_up/test11.js
@@ -1,3 +1,17 @@
 #!/usr/bin/node
 const args = process.argv;
-console.log(args[2] + ' is ' + args[3]);
+
+if(args.lenght <= 3) {
+  console.log(1);
+} else {
+  let max1 = args[2];
+  let max2 = args[3];
+  for(let i = 3; i < args.lenght; i++)
+  {
+    if(max1 < args[i]){
+      max2 = max1;
+      max1 = args[i];
+    }
+  }
+  console.log(max2);
+}
