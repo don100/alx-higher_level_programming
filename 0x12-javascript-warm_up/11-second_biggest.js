a2c96bb598ca9f66cb23cec898b61b2cce5fdcde
don100 <hajjaj.chiheb1@gmail.com>
2023-11-18 18:23:35 +0100

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,7 @@ if (args.length <= 3) {
   console.log(0);
 } else {
   let max1 = parseInt(args[2]);
-  let max2 = parseInt(args[2]);
+  let max2 = parseInt(args[3]);
   for (let i = 3; i < args.length; i++) {
     if (max2 < parseInt(args[i])) {
       if (max1 < parseInt(args[i])) {
