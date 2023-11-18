f28c49173b112d02513bb709a2b3b3496ceebf9d
don100 <hajjaj.chiheb1@gmail.com>
2023-11-18 17:41:26 +0100

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 const args = process.argv;
 
-if(args.length <= 3) {
+if (args.length <= 3) {
   console.log(0);
 } else {
   let max1 = args[2];
   let max2 = args[3];
-  for(let i = 3; i < args.length; i++)
-  {
-    if(max1 < args[i]){
+  for (let i = 3; i < args.length; i++) {
+    if (max1 < args[i]) {
       max2 = max1;
       max1 = args[i];
     }
