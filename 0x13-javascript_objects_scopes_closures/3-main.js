279400f811749521ca3b2a0692bc547dfce8891e
don100 <hajjaj.chiheb1@gmail.com>
2023-11-19 15:10:06 +0100

    Update 3-main.js

--- 0x13-javascript_objects_scopes_closures/3-main.js
+++ 0x13-javascript_objects_scopes_closures/3-main.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
+const Rectangle = require('./3-rectangle');
+
+const r1 = new Rectangle(2, 3);
+r1.print();
+
+const r2 = new Rectangle(10, 5);
+r2.print();
