b265db013a0c7fdcfb20047f16e254480e47416a
don100 <hajjaj.chiheb1@gmail.com>
2023-11-19 16:22:32 +0100

    Update 4-main.js

--- 0x13-javascript_objects_scopes_closures/4-main.js
+++ 0x13-javascript_objects_scopes_closures/4-main.js
@@ -1,3 +1,14 @@
 #!/usr/bin/node
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
+const Rectangle = require('./4-rectangle');
+
+const r1 = new Rectangle(2, 3);
+console.log('Normal:');
+r1.print();
+
+console.log('Double:');
+r1.double();
+r1.print();
+
+console.log('Rotate:');
+r1.rotate();
+r1.print();
