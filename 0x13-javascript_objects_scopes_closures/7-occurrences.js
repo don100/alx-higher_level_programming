e0cb6d52c34b073dc75ba8a949c3ddfd2d1007ec
don100 <hajjaj.chiheb1@gmail.com>
2023-11-19 17:35:53 +0100

    Update 7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-    let nbOccurences = 0;
-    for (let i of list) {
-        if (i === searchElement) {
-            nbOccurences += 1;
-        }
+let nbOccurences = 0;
+for (let i of list) {
+    if (i === searchElement) {
+        nbOccurences += 1;
     }
-    return nbOccurences;
+}
+return nbOccurences;
 };
