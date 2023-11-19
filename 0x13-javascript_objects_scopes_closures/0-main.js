eb9d607bfdcb07c64a0d2c43206215a862539b1c
don100 <hajjaj.chiheb1@gmail.com>
2023-11-19 13:23:09 +0100

    Update 0-main.js

--- 0x13-javascript_objects_scopes_closures/0-main.js
+++ 0x13-javascript_objects_scopes_closures/0-main.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
+const Rectangle = require('./0-rectangle');
+
+const r1 = new Rectangle();
+console.log(r1);
+console.log(r1.constructor);
