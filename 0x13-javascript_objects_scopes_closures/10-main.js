39300aaf26dd7ca5112ab5f21685cf3a8e93ebfd
don100 <hajjaj.chiheb1@gmail.com>
2023-11-19 21:08:19 +0100

    Update 10-main.js

--- 0x13-javascript_objects_scopes_closures/10-main.js
+++ 0x13-javascript_objects_scopes_closures/10-main.js
@@ -1,3 +1,15 @@
 #!/usr/bin/node
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
+const converter = require('./10-converter').converter;
+
+let myConverter = converter(10);
+
+console.log(myConverter(2));
+console.log(myConverter(12));
+console.log(myConverter(89));
+
+
+myConverter = converter(16);
+
+console.log(myConverter(2));
+console.log(myConverter(12));
+console.log(myConverter(89));
