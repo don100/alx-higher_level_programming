c0aae017ec5ec5408c742b9fd0bc9b51ed638eb8
don100 <hajjaj.chiheb1@gmail.com>
2023-11-19 21:32:30 +0100

    Update 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const listEsrever = [];
-  for (const i of list) {
+  for (const i in list) {
     listEsrever.unshift(i);
   }
   return listEsrever;
