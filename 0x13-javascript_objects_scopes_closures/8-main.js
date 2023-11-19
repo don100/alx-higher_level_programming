42aa1fe67feacde63e45f8a3116a7ee7ee49dfdc
don100 <hajjaj.chiheb1@gmail.com>
2023-11-19 18:11:50 +0100

    Update 8-main.js

--- 0x13-javascript_objects_scopes_closures/8-main.js
+++ 0x13-javascript_objects_scopes_closures/8-main.js
@@ -1,3 +1,5 @@
 #!/usr/bin/node
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
+const esrever = require('./8-esrever').esrever;
+
+console.log(esrever([1, 2, 3, 4, 5]));
+console.log(esrever(["School", 89, { id: 12 }, "String"]));
