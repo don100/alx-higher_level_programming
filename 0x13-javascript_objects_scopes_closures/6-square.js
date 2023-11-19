32fe90a2dd5e3af9570c76f2d5fd0c4757d2942c
don100 <hajjaj.chiheb1@gmail.com>
2023-11-19 16:53:25 +0100

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,15 +4,4 @@ module.exports = class Square extends Rectangle {
   constructor (size) {
     super (size, size);
   }
-
-  charPrint (c = 'X') {
-    let side = '';
-    for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) {
-        side += c;
-      }
-      console.log(side);
-      side = '';
-    }
-  }
 };
