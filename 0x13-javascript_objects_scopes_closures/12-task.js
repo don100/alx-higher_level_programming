529e42ff253acc6e9a04f1342aaa0cd36b86c02a
don100 <hajjaj.chiheb1@gmail.com>
2023-11-20 21:02:36 +0100

    Update 12-task.js

--- 0x13-javascript_objects_scopes_closures/12-task.js
+++ 0x13-javascript_objects_scopes_closures/12-task.js
@@ -1,3 +1,17 @@
 #!/usr/bin/node
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
+const dict = require('./101-data').dict;
+
+let v = [...new Set(Object.values(dict))];
+let a = [];
+let d = {};
+for (let i of v){
+  for (let key in dict) {
+    //console.log("Key: " + key + ", Value: " + dict[key]);
+    if (dict[key] == i){
+      a.push(key);
+    }
+  }
+  d[i] = a;
+  a = [];
+}
+console.log(d);
