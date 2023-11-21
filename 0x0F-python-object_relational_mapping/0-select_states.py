52c63cfe8a031c4625287fe1b919c51f541405ab
don100 <hajjaj.chiheb1@gmail.com>
2023-11-22 00:15:31 +0100

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,20 +7,17 @@ MY_USER = sys.argv[1]
 MY_PASS = sys.argv[2]
 MY_DB = sys.argv[3]
 
-db = MySQLdb.connect(host=MY_HOST,    # your host, usually localhost
-                     user=MY_USER,         # your username
-                     passwd=MY_PASS,  # your password
-                     db=MY_DB)        # name of the data base
+db = MySQLdb.connect(host=MY_HOST,
+                     user=MY_USER,
+                     passwd=MY_PASS,
+                     db=MY_DB)
 
-# you must create a Cursor object. It will let
-#  you execute all the queries you need
 cur = db.cursor()
 
-# Use all the SQL you like
 cur.execute("SELECT * FROM states ORDER BY states.id ASC")
 
-# print all the first cell of all the rows
 for row in cur.fetchall():
     print(f"({row[0]}, '{row[1]}')")
 
 db.close()
+
