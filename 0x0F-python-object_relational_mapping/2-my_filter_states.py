a6c2fb2913ae77720475727332bcc0e143db9de5
don100 <hajjaj.chiheb1@gmail.com>
2023-11-22 15:58:46 +0100

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -17,9 +17,9 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    cur.execute(f"SELECT * FROM states \
-    WHERE states.name LIKE '{sys.argv[4]}' \
-    ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM states \
+    WHERE states.name LIKE '{}' \
+    ORDER BY states.id ASC".format(sys.argv[4]))
 
     for row in cur.fetchall():
         print(row)
