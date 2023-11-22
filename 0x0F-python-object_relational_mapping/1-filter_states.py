5419d6f29f68eccb3736787c0c455b85e5d48b1e
don100 <hajjaj.chiheb1@gmail.com>
2023-11-22 01:50:54 +0100

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -18,8 +18,8 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states 
-                WHERE name LIKE 'N%' 
-                ORDER BY states.id ASC")
+    WHERE name LIKE 'N%' 
+    ORDER BY states.id ASC")
 
     for row in cur.fetchall():
         print(row)
