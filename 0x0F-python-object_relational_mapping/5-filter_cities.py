964db9bea3b7e27be8b8142573b714e61b35d8a6
don100 <hajjaj.chiheb1@gmail.com>
2023-11-24 20:22:38 +0100

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     cur.execute("SELECT cities.id, cities.name, states.name FROM cities \
-    LEFT JOIN states ON cities.state_id=states.id WHERE states.name LIKE BINAREY {} ORDER BY cities.id ASC".format(sys.argv[4]))
+    LEFT JOIN states ON cities.state_id=states.id WHERE states.name LIKE BINAREY '{}' ORDER BY cities.id ASC".format(sys.argv[4]))
 
     for row in cur.fetchall():
         print(row[1])
