9b601f7e65bcb92035e0e27303235923e7a0dc10
don100 <hajjaj.chiheb1@gmail.com>
2023-11-24 20:39:26 +0100

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    cur.execute("SELECT cities.id, cities.name, states.name\
+    cur.execute("SELECT cities.id, cities.name, states.name \
     FROM cities \
     LEFT JOIN states \
     ON cities.state_id=states.id \
