a4291674d202739d1a69d6bc2f63229f390f95b9
don100 <hajjaj.chiheb1@gmail.com>
2023-11-24 19:58:53 +0100

    Update 4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM cities LEFT JOIN states ON \
-    cities.id=states.id ORDER BY cities.id ASC")
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON \
+    cities.state_id=states.id ORDER BY cities.id ASC")
 
     for row in cur.fetchall():
         print(row)
