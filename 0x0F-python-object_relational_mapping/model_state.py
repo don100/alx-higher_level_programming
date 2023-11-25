7aa106334835c716c9a89ec730c2956e099a8ff4
don100 <hajjaj.chiheb1@gmail.com>
2023-11-26 00:50:54 +0100

    Update model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,7 +11,7 @@ class State(Base):
     """ class """
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer(11), primary_key=True)
     name = Column(String(128), nullable=False)
 
     def __repr__(self):
