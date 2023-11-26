da267c1f52bee83652da80dde19ddcb963253405
don100 <hajjaj.chiheb1@gmail.com>
2023-11-26 20:42:44 +0100

    Update 9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                                   sys.argv[3]), pool_pre_ping=True)
     """Base.metadata.create_all(engine)"""
     session = Session(engine)
-    for state in session.query(State).order_by(State.id).\
-                 filter(State.name.like('%a%')):
+    for state in session.query(State).order_by(State.id).filter(
+                                               State.name.like('%a%')):
         print("{}: {}".format(state.id, state.name))
     session.close()
