eb1f9e496b8820fe4ef62481fd661547f9604a70
don100 <hajjaj.chiheb1@gmail.com>
2023-11-26 20:51:08 +0100

    Update 10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -12,7 +12,10 @@ if __name__ == "__main__":
                                   sys.argv[3]), pool_pre_ping=True)
     """Base.metadata.create_all(engine)"""
     session = Session(engine)
-    for state in session.query(State).order_by(State.id).filter(
-                                               State.name.like('%a%')):
-        print("{}: {}".format(state.id, state.name))
+    count = session.query(State).order_by(State.id).filter(
+                                               State.name.like(sys.argv[4])).count()
+    if count:
+        print("{}".format(count))
+    else:
+        print('Not found')
     session.close()
