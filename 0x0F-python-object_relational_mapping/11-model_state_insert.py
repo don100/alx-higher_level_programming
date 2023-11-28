406d1155b55982ffbb7e32d1a985181b7d7d54a4
don100 <hajjaj.chiheb1@gmail.com>
2023-11-29 00:31:31 +0100

    Update 11-model_state_insert.py

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -12,6 +12,6 @@ if __name__ == "__main__":
                                   sys.argv[3]), pool_pre_ping=True)
     """Base.metadata.create_all(engine)"""
     session = Session(engine)
-    state = insert(State).values(name="Louisiana",)
+    state = insert(states).values(name="Louisiana",)
     print(state.id)
     session.close()
